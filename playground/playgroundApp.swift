9fb82ee213abd02429020f717529190460a34754
Hao Mun CHEN <hao.mun.chen@comerge.net>
2024-04-16 14:18:39 +0800

    Initial Commit

---
+++ playground/playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  playgroundApp.swift
+//  playground
+//
+//  Created by Hao Mun Chen on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
